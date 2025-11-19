f7ecc3fbacd7f1acc1a2c9704da07775be354eb9
koek1 <geandredupreez1@gmail.com>
2025-11-19 08:30:39 +0200

    feat: Started implementing UI

---
+++ frontend/lib/utils/helpers.dart
@@ -0,0 +1,15 @@
+import 'package:intl/intl.dart';
+
+class Helpers {
+    static String formatCurrency(double amount) {
+        return NumberFormat.currency(symbol: '\R', decimalDigits: 2).format(amount);
+    }
+
+    static String FormatDate(DateTime date) {
+        return DateFormat('dd, MMM, yyyy').format(date);
+    }
+
+    static String formatDateForAPI(DateTime date) {
+        return DateFormat('yyyy-MM-dd;').format(date);
+    }
+}
